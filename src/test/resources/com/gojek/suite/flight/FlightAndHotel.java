cb5d0187079bcb95181b798aa5d97d5470e2711e
Admin@7002 <--global>
2022-12-11 19:05:30 +0530

    gateway project

---
+++ src/test/resources/com/gojek/suite/flight/FlightAndHotel.java
@@ -0,0 +1,21 @@
+package test.resources.com.gojek.suite.flight;
+
+import org.testng.annotations.Test;
+
+public class FlightAndHotel 
+
+
+{
+
+	@Test
+	public void function1()
+	{
+		
+		System.out.println("Neelam");
+		//MultiCity mc=new MultiCity();
+		//mc.automateMultiCity();
+		
+		
+	}
+	
+}
